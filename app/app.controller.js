75827835703d39910333ec6714cdbbe218acec40
Kacper <kacper@traffit.com>
2017-04-06 18:25:59 +0200

    mobile about, cameras not block init screen

--- app/app.controller.js
+++ app/app.controller.js
@@ -1,18 +1,33 @@
 'use strict';
 
 var pgsApp = angular.module('pgsApp');
-pgsApp.controller('appController',['$timeout', 'skiService', function($timeout, skiService){
+pgsApp.controller('appController',['skiService', function(skiService){
   var scope = this;
   scope.camData = {};
 
-  $timeout(function(){
+  var generateKeyObject = function(data){
+    var ret = {};
+    for(var i in data){
+      if(!data.hasOwnProperty(i)) continue;
+
+      var key = data[i].name;
+      ret[key] = data[i];
+    }
+
+    return ret;
+  };
+
+  var init = function init(){
     skiService.getSkiCams().then(function getSkiCamSuccess(ret){
-      scope.systemInited = true;
-      angular.extend(scope.camData, ret.data);
-      console.log(scope.camData);
+      angular.extend(scope.camData, generateKeyObject(ret.data));
+      scope.camData.asyncLoaded = true;
     },function getSkiCamError(){
-      //TODO
-      scope.systemInited = true;
+      scope.camData.asyncLoaded = true;
+      scope.camData.fetchError = true;
     });
-  })
+    
+    scope.systemInited = true;
+  };
+
+  setTimeout(init);
 }]);
